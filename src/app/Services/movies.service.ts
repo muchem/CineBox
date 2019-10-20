03f9f06f5a3c954ffb79fdb4beace0f0b66065b2
muchemm <melissauchem@gmail.com>
2019-10-19 18:37:38 -0700

    add popular movie method to Service.

--- src/app/Services/movies.service.ts
+++ src/app/Services/movies.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
   constructor(private http:HttpClient) { }
+  apiKey: string = environment.API_Key;
   getUpcomingMovies():Observable<any[]>{
-    return this.http.get<any[]>(`https://api.themoviedb.org/3/movie/upcoming?api_key=43c00618f867c060677c3dcc6de93669&language=en-US&page=1`);
+    return this.http.get<any[]>(`https://api.themoviedb.org/3/movie/upcoming?api_key=${this.apiKey}&language=en-US&page=1`);
+  }
+  getPopularMovies():Observable<any[]>{
+    return this.http.get<any[]>(`https://api.themoviedb.org/3/movie/popular?api_key=${this.apiKey}&language=en-US&page=1`);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_Key: '43c00618f867c060677c3dcc6de93669'
 };
 
 /*
